f92923be3b1c735a7325e0bc3198f3a96f5358ac
Ibrahim Al-Ashhab <s3953973@student.rmit.edu.au>
2023-04-16 23:37:21 +1000

    generate data file and generated data

--- generateData.py
+++ generateData.py
@@ -14,21 +14,31 @@ while validInp != True:
         size = random.randint(100, 500)
         validInp = True
     elif userInp == "medium":
-        size = random.randint(1000, 5000)
+        size = random.randint(1000, 2500)
         validInp = True
     elif userInp == "large":
-        size = random.randint(10000, 50000)
+        size = random.randint(5000, 10000)
         validInp = True
+size = 8313
+print(size)
+# numValCells = random.randint(0, size*size)
 
-numIters = random.randint(0, size*size)
-f = open("generatedData.txt", "w")
-while numIters != 0:
+print("Please enter the precentage of non-None cells in the dataset:")
+print("(enter a percentage between 0 and 1)")
+valPercentage = input()
+
+numValCells = round(float(valPercentage) * (size*size))
+f = open("large-0.9-data.txt", "w")
+f.write(str(size) + " " + str(size) + " " +
+        str(round(random.uniform(-9.99, 10), 2)) + "\n")
+numValCells -= 1
+
+while numValCells != 0:
     randRow = random.randint(0, size-1)
     randCol = random.randint(0, size-1)
     if (randRow, randCol) not in cellSet:
-        # print(randRow, randCol, round(random.uniform(-9.99,10), 2))
         f.write(str(randRow) + " " + str(randCol) + " " +
                 str(round(random.uniform(-9.99, 10), 2)) + "\n")
         cellSet.add((randRow, randCol))
-        numIters -= 1
+        numValCells -= 1
 f.close()
